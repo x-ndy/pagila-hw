6f6a1bc8cbafb5b8e039722e58e93962199020c3
xandy <aseow23@cmc.edu>
2023-02-21 00:05:20 -0800

    please work

--- sql/09.sql
+++ sql/09.sql
@@ -6,5 +6,5 @@
 SELECT first_name, last_name, sum(amount) AS sum 
 FROM staff 
 JOIN payment ON staff.staff_id=payment.staff_id 
-WHERE payment.payment_date BETWEEN '2020-01-01' AND '2020-02-01' 
+WHERE payment.payment_date BETWEEN '2019-12-31' AND '2020-02-01' 
 GROUP BY staff.staff_id, staff.first_name, staff.last_name;
